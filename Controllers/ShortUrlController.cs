e143bd4755b64e9c7e26caf7872f0c062c0eccd8
Yeshal Khan <yeshalkhan13@gmail.com>
2025-03-23 20:45:22 +0500

    Created ShortUrl controller

---
+++ Controllers/ShortUrlController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlShorteningService.Data;
+
+namespace UrlShorteningService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShortUrlController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShortUrlController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
