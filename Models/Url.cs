3e4cd38e49fe143004ac9b499164601cc755aed1
Yeshal Khan <yeshalkhan13@gmail.com>
2025-03-23 20:34:06 +0500

    Added Shorturl model

---
+++ Models/Url.cs
@@ -0,0 +1,12 @@
+﻿namespace UrlShorteningService.Models
+{
+    public class ShortUrl
+    {
+        public int Id { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public string ShortCode { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public int AccessCount { get; set; } = 0;
+    }
+}
